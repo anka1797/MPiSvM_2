3c100709ae508034487724a399b64e7bc4c300d9
anka1797 <annn012001@mail.ru>
2022-12-17 22:33:43 +0500

    создание библиотеки, вывод результатов

---
+++ labwork/labwork2_web/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using labwork2_web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using teploobmenLibrary;
+
+namespace labwork2_web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult Index(TeploobmenInput input)
+        {
+            var lib = new Teploobmen(input);
+            var resualt = lib.calc();
+            return View(resualt);
+        }
+
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
